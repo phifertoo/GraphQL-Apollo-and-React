93f89460752dfd0542d67f91d8ef07b5e5e69a71
phifertoo <phiferthree@gmail.com>
2020-07-08 14:37:00 -0700

    client folder added

--- server.js
+++ server.js
@@ -1,17 +1,21 @@
 const express = require('express');
 const { graphqlHTTP } = require('express-graphql');
-const schema = require('./schema')
-
+const schema = require('./schema');
+const cors = require('cors');
 const app = express();
+
+//allow cross-origin
+
+app.use(cors());
 
 app.use(
   '/graphql',
   graphqlHTTP({
     schema,
     graphiql: true,
-  }),
+  })
 );
 
 const PORT = process.env.PORT || 5000;
 
-app.listen(PORT, ()=>console.log(`server started on ${PORT}`));
+app.listen(PORT, () => console.log(`server started on ${PORT}`));
